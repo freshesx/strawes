20c63a73b3d9fc4dc844c1e8b1640f1b8bc506de
ChaselWu <minowu@foxmail.com>
2017-08-06 14:27:04 +0800

    Update example

--- examples/components/dashboard/index.vue
+++ examples/components/dashboard/index.vue
@@ -1,5 +1,25 @@
 <template>
-  <div>
-    dashboard
-  </div>
+  <mw-frame>
+    <mw-brand slot="brand">
+      Strawes
+    </mw-brand>
+    <mw-navigation slot="navigation"></mw-navigation>
+    <div slot="information">
+      ChaselWu
+    </div>
+  </mw-frame>
 </template>
+
+<script>
+  import frame from '../../../suites/frame'
+  import brand from '../../../suites/brand'
+  import navigation from '../../../suites/navigation'
+
+  export default {
+    components: {
+      ...frame.map(),
+      ...brand.map(),
+      ...navigation.map()
+    }
+  }
+</script>
