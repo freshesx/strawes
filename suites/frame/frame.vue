94df56438e330676bd6fc04b2588fca71ff55b31
ChaselWu <minowu@foxmail.com>
2017-08-06 14:12:50 +0800

    New frame component

---
+++ suites/frame/frame.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="mw-frame">
+    <div class="mw-frame-side">
+      <div class="mw-frame-side-inner">
+        <div class="mw-frame-brand">
+          <slot name="brand"></slot>
+        </div>
+        <div class="mw-frame-navigation">
+          <slot name="navigation"></slot>
+        </div>
+        <div class="mw-frame-information">
+          <slot name="information"></slot>
+        </div>
+      </div>
+    </div>
+    <div class="mw-frame-contents">
+      <slot name="contents"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Element from 'vue-human/utils/Element'
+
+  export default new Element({
+    name: 'mw-frame'
+  })
+</script>
+
+<style lang="scss">
+  .mw-frame {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    display: flex;
+  }
+
+  .mw-frame-side {
+    width: 220px;
+    flex-shrink: 0;
+  }
+
+  .mw-frame-side-inner {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    background: #fff;
+    box-shadow: 4px 0 32px rgba(0, 0, 0, 0.02);
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
+
+  .mw-frame-brand {
+    // @todo add to brand
+    // padding: 1rem;
+    // font-size: 1.25rem;
+    flex-shrink: 0;
+  }
+
+  .mw-frame-navigation {
+    flex: 1;
+  }
+
+  .mw-frame-information {
+    flex-shrink: 0;
+  }
+
+  .mw-frame-contents {
+    flex: 1;
+  }
+</style>
---
+++ suites/frame/index.js
@@ -0,0 +1,6 @@
+import Suites from 'vue-human/utils/Suites'
+import frame from './frame'
+
+export default new Suites([
+  frame
+])
