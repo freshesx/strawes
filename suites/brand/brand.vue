ed295786c19ed05acf91609b8df5c776b8e04aa9
ChaselWu <minowu@foxmail.com>
2017-08-06 14:12:41 +0800

    New brand component

---
+++ suites/brand/brand.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="sw-brand">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  import Element from 'vue-human/utils/Element'
+
+  export default new Element({
+    name: 'sw-brand'
+  })
+</script>
+
+<style lang="scss" scoped>
+  .sw-brand {
+    padding: 1rem;
+    font-size: 1.25rem;
+  }
+</style>
---
+++ suites/brand/index.js
@@ -0,0 +1,6 @@
+import Suites from 'vue-human/utils/Suites'
+import brand from './brand'
+
+export default new Suites([
+  brand
+])
