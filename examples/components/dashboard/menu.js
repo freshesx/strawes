291add64f24e31413984b6354a114cb1ab2c9287
weiwei <weiwei@suiyi.com.cn>
2017-08-08 13:46:56 +0800

    Customize icon and show arrow icon in first-level which has children

--- examples/components/dashboard/menu.js
+++ examples/components/dashboard/menu.js
@@ -9,26 +9,27 @@
 export default [
   {
     label: '首页',
-    route: { name: 'dashboard' }
+    route: { name: 'dashboard' },
+    icon: require('vue-human-icons/js/ios/gear')
   },
   {
     label: '原则',
     children: [
       {
         label: '规则化',
-        route: { name: 'stylesRegulation' }
+        route: { name: 'dashboard' }
       },
       {
         label: '多屏化',
-        route: { name: 'stylesMultiscreens' }
+        route: { name: 'dashboard' }
       },
       {
         label: '高效化',
-        route: { name: 'stylesEfficient' }
+        route: { name: 'dashboard' }
       },
       {
         label: '活泼化',
-        route: { name: 'stylesLively' }
+        route: { name: 'dashboard' }
       }
     ]
   }
--- suites/menu/menu.vue
+++ suites/menu/menu.vue
@@ -3,22 +3,27 @@
     <div class="mw-menu-first-level" v-for="item in list">
       <!-- 父菜单 -->
       <div class="mw-menu-first-button">
-        <a href="#" class="mw-menu-first-link">
+        <router-link :to="item.route" class="mw-menu-first-link" v-if="item.route">
+          <mn-icon :name="item.icon" :scale="0.9" v-if="item.icon"></mn-icon>
           {{ item.label }}
-        </a>
+        </router-link>
+        <span v-else class="mw-menu-first-link">
+          <mn-icon :name="item.icon" :scale="0.9" v-if="item.icon"></mn-icon>
+          {{ item.label }}
+        </span>
         <div class="mw-menu-badge">
 
         </div>
-        <div class="mw-menu-dropdown">
-
+        <div class="mw-menu-dropdown" v-if="item.children && item.children.length > 0">
+          <mn-icon :name="icons.arrowDown" :scale="0.9"></mn-icon>
         </div>
       </div>
       <!-- 子菜单 -->
-      <div class="mw-menu-second-level" v-if="item.children && item.children.length > 0">
+      <div class="mw-menu-second-level is-hidden" v-if="item.children && item.children.length > 0">
         <div class="mw-menu-second-button" v-for="child in item.children">
-          <a href="#" class="mw-menu-second-link">
+          <router-link :to="child.route" class="mw-menu-second-link">
             {{ child.label }}
-          </a>
+          </router-link>
         </div>
       </div>
     </div>
@@ -28,10 +33,21 @@
 <script>
   import Element from 'vue-human/utils/Element'
 
+
   export default new Element({
     name: 'mw-menu',
     props: {
       list: Array
+    },
+    data () {
+      return {
+        icons: {
+          arrowDown: require('vue-human-icons/js/ios/arrow-down'),
+          arrowUp: require('vue-human-icons/js/ios/arrow-up')
+        }
+      }
+    },
+    methods: {
     }
   })
 </script>
@@ -47,6 +63,7 @@
 
   .mw-menu-first-button {
     display: flex;
+    align-items: center;
     transition-duration: 500ms;
 
     &:hover {
@@ -56,10 +73,15 @@
 
   .mw-menu-first-link {
     display: block;
+    align-items: center;
     flex: 1;
     padding: 0.5rem 1rem;
     text-decoration: none;
     color: #333;
+
+    .mn-icon {
+      margin-right: 0.2rem;
+    }
   }
 
   .mw-menu-badge {
@@ -79,6 +101,10 @@
   }
 
   .mw-menu-second-link {
-
+    display: block;
+    flex: 1;
+    padding: 0.5rem 2rem;
+    text-decoration: none;
+    color: #333;
   }
 </style>
