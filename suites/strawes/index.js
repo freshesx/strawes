e1e0e3e800e5155ddaff4f94490e12bba8100134
ChaselWu <minowu@foxmail.com>
2017-08-19 19:38:24 +0800

    New strawes Suites

---
+++ suites/strawes/index.js
@@ -0,0 +1,18 @@
+import Suites from 'vue-human/utils/Suites'
+import brand from '../brand'
+import container from '../container'
+import crumb from '../crumb'
+import frame from '../frame'
+import menu from '../menu'
+import navigation from '../navigation'
+import table from '../table'
+
+export default new Suites([
+  brand,
+  container,
+  crumb,
+  frame,
+  menu,
+  navigation,
+  table
+])
