2126deae28ea76d80b2799e5b41a004f32b70645
ChaselWu <minowu@foxmail.com>
2017-08-19 19:38:37 +0800

    Use strawes in human.js

--- examples/human/index.js
+++ examples/human/index.js
@@ -2,8 +2,10 @@ import Vue from 'vue'
 import Human from 'vue-human'
 import locales from '../locale'
 import core from 'vue-human/suites/core'
+import strawes from '../../suites/strawes'
 import './app.scss'
 
 // Use Human
 Vue.use(Human, { locales })
 Vue.use(core)
+Vue.use(strawes)
