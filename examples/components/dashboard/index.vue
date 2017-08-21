929bb812a36d163d4cfb5ea4dbb1dd2115d3f27a
ChaselWu <minowu@foxmail.com>
2017-08-21 22:31:50 +0800

    Remove old imported resource component

--- examples/components/dashboard/index.vue
+++ examples/components/dashboard/index.vue
@@ -15,18 +15,7 @@
 </template>
 
 <script>
-  import frame from '../../../suites/frame'
-  import brand from '../../../suites/brand'
-  import navigation from '../../../suites/navigation'
-  import menu from '../../../suites/menu'
-
   export default {
-    components: {
-      ...frame.map(),
-      ...brand.map(),
-      ...navigation.map(),
-      ...menu.map()
-    },
     data () {
       return {
         menu: require('./menu').default
