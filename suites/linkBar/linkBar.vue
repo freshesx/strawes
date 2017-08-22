bb9ba29ab8b3f6005ed84d9579306be1b51b37b4
ChaselWu <minowu@foxmail.com>
2017-08-22 12:53:24 +0800

    Update linkBar: add click and max

--- suites/linkBar/linkBar.vue
+++ suites/linkBar/linkBar.vue
@@ -8,19 +8,19 @@
       size="sm"
       :icon="action.icon"
       :href="action.href"
-      :to="action.to"></mn-link>
+      :to="action.to"
+      @click="onAction(arguments[0], arguments[1], action)"></mn-link>
     <mn-btn class="has-none-padding-x"
       theme="secondary-link"
       :icon="more"
       size="sm"
       @click.stop.prevent="onMore"
-      v-if="actions.length > 3"></mn-btn>
+      v-if="actions.length > max"></mn-btn>
     <!-- Action Sheet -->
     <mn-action-sheet :visible.sync="showActionSheet" v-if="actions.length > 3">
       <mn-card rounded>
         <mn-card-btns>
           <button v-for="action in actions">
-            <mn-icon :name="action.icon"></mn-icon>
             {{ action.label }}
           </button>
         </mn-card-btns>
@@ -39,7 +39,11 @@
       ...actionSheet.map()
     },
     props: {
-      actions: Array
+      actions: Array,
+      max: {
+        type: Number,
+        default: 3
+      }
     },
     data () {
       return {
@@ -50,20 +54,17 @@
     computed: {
       expandedActions () {
         if (!Array.isArray(this.actions)) return []
-        if (this.actions.length > 3) return this.actions.slice(0, 2)
+        if (this.actions.length > this.max) return this.actions.slice(0, this.max - 1)
         return this.actions
       },
-      collapsedActions () {
-        if (Array.isArray(this.actions) && this.actions.length > 3) {
-          return this.actions.slice(2, this.actions.length)
-        }
-        return []
-      }
     },
     methods: {
       onMore () {
-        console.log('nihao')
         this.showActionSheet = true
+      },
+      onAction (event, button, action) {
+        this.$emit('actions', event, button, action)
+        this.$emit(`action:${action.name}`, event, button, action)
       }
     }
   })
