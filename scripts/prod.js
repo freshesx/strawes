cebf2c54941400443d404f65492d13cb8a35b29f
ChaselWu <minowu@foxmail.com>
2017-08-30 14:48:33 +0800

    Change prod folder

--- scripts/prod.js
+++ scripts/prod.js
@@ -1,6 +1,11 @@
 const WebpackProdRunner = require('freshes-man/helpers/WebpackProdRunner')
+const path = require('path')
+
 module.exports = new WebpackProdRunner({
   cssSourceMap: false,
   cssMinimize: true,
   cssExtract: true,
+  appPath: path.join(process.cwd(), 'examples'),
+  testPath: path.join(process.cwd(), 'examples/test'),
+  sassResources: path.join(process.cwd(), 'examples/human/config.scss')
 }).run()
